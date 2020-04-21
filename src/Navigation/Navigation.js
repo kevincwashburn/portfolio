573572deddbab53a1d565134afdb6da4177e5a49
Kevin Washburn <kevincwashburn@gmail.com>
2020-04-21 16:35:00 -0400

    added navbar files

---
+++ src/Navigation/Navigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+import 
+
+
+
+<AppBar position="static">
+  <Toolbar>
+    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6" className={classes.title}>
+      News
+    </Typography>
+    <Button color="inherit">Login</Button>
+  </Toolbar>
+</AppBar>
